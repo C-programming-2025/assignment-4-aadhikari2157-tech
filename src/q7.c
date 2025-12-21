54afad89be8ac291c587c5be8d8f7d4b50aa8343
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-12-21 09:55:47 +0545

    q7.c

--- src/q7.c
+++ src/q7.c
@@ -1,2 +1,25 @@
-// Write a C program that defines a structure Employee with members: name, employee ID, and salary. 
-// Take input for 5 employees and display the details of the employee with the highest salary.
+#include <stdio.h>
+
+struct Employee {
+    char name[50];
+    int id;
+    float salary;
+};
+
+int main() {
+    struct Employee e[5];
+    int i, max = 0;
+
+    for (i = 0; i < 5; i++) {
+        printf("\nEmployee %d\n", i + 1);
+        scanf("%s %d %f", e[i].name, &e[i].id, &e[i].salary);
+        if (e[i].salary > e[max].salary)
+            max = i;
+    }
+
+    printf("\nHighest Salary Employee:\n");
+    printf("Name: %s\nID: %d\nSalary: %.2f",
+           e[max].name, e[max].id, e[max].salary);
+
+    return 0;
+}
