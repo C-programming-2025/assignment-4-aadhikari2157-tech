c671baeb8cf601da20fae61bc96ce1dc1de692f7
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-12-21 09:51:27 +0545

    q4.c

--- src/q4.c
+++ src/q4.c
@@ -1,1 +1,30 @@
-// Write a C program that dynamically allocates memory for an integer array of size n, where n is entered by the user. Prompt the user to enter n elements and display them using pointers.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100]; 
+    int *ptr;
+
+    printf("Enter the number of elements (max 100): ");
+    scanf("%d", &n);
+
+    if (n > 100) {
+        printf("Too many elements.\n");
+        return 1;
+    }
+
+    ptr = arr;
+
+    for (i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", ptr + i);
+    }
+
+    printf("The elements are: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", *(ptr + i));
+    }
+    printf("\n");
+
+    return 0;
+}
