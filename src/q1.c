5a08437d7cd1c1471cd88add54661442ebbbb385
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-12-21 09:25:09 +0545

    q1.c

--- src/q1.c
+++ src/q1.c
@@ -1,1 +1,19 @@
-// Write a C program that swaps the values of two integers using pointers.
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+    int *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    p1 = &a;
+    p2 = &b;
+
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    printf("After swapping: a = %d, b = %d", a, b);
+    return 0;
+}
